f2d52cd31e86e2d57e7d8556f466f0e38a183180
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-03-11 11:51:19 -0500

    added validation, but code doesn't work

--- scriptHelper.js
+++ scriptHelper.js
@@ -18,6 +18,39 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
+    //maybe get info from form submission
+    //need to validat pilot, co-pilot, fuel level, cargo mass
+    <script>
+    window.addEventListener("load", function() {
+        let form = document.querySelector("formSubmission");
+        form.addEventListener("submit", function(event) {
+        let pilot = document.querySelector("input[name=pilot]");
+        let copilot = document.querySelector("input[name=copilot]");
+        let fuelLevel = document.querySelector("input[quantity= fuelLevel]");
+        let cargoLevel = document.querySelector("input[quantity = cargoLevel]");
+
+        if (pilot.value === "" || copilot.value === "" || fuelLevel === ""|| cargoLevel ==="") {
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+        } else if(pilot.value === isNaN || copilot.value === isNaN){
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+            return("Not a number");
+
+        } else(fuelLevel.value === !isNaN || cargoLevel.value === !isNaN){
+            alert("All fields are required!");
+            // stop the form submission
+            event.preventDefault();
+            return("Is a number");
+        }
+        });
+    });
+</script>
+
+    //start coding here   isNaN(value)
+    //make in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate. 
     
  }
  
