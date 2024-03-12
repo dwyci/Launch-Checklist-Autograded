caaa20c5087298dd312491b055467438f743ef8b
Dallas Wyciskalla <dallas.wyciskalla@gmail.com>
2024-03-11 19:43:43 -0500

    part 2 completed

--- script.js
+++ script.js
@@ -10,7 +10,10 @@ window.addEventListener("load", function() {
         console.log(listedPlanets);
     }).then(function () {
         console.log(listedPlanets);
-        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+        // Below this comment call the appropriate helper functions to pick a planet fom the list of planets 
+        //and add that information to your destination.
+
+        
     })
     
  });
--- scriptHelper.js
+++ scriptHelper.js
@@ -18,44 +18,36 @@ function addDestinationInfo(document, name, diameter, star, distance, moons, ima
  }
  
  function validateInput(testInput) {
-    //maybe get info from form submission
-    //need to validat pilot, co-pilot, fuel level, cargo mass
-    <script>
-    window.addEventListener("load", function() {
-        let form = document.querySelector("formSubmission");
-        form.addEventListener("submit", function(event) {
-        let pilot = document.querySelector("input[name=pilot]");
-        let copilot = document.querySelector("input[name=copilot]");
-        let fuelLevel = document.querySelector("input[quantity= fuelLevel]");
-        let cargoLevel = document.querySelector("input[quantity = cargoLevel]");
-
-        if (pilot.value === "" || copilot.value === "" || fuelLevel === ""|| cargoLevel ==="") {
-            alert("All fields are required!");
-            // stop the form submission
-            event.preventDefault();
-        } else if(pilot.value === isNaN || copilot.value === isNaN){
-            alert("All fields are required!");
-            // stop the form submission
-            event.preventDefault();
-            return("Not a number");
-
-        } else(fuelLevel.value === !isNaN || cargoLevel.value === !isNaN){
-            alert("All fields are required!");
-            // stop the form submission
-            event.preventDefault();
-            return("Is a number");
+        if(testInput ===""){
+            return "Empty";
+        } else if(isNaN(testInput)){
+            return "Not a number";
+        } else{
+            return "Is a number";
         }
-        });
-    });
-</script>
 
-    //start coding here   isNaN(value)
-    //make in a string as a parameter and return "Empty", "Not a Number", or "Is a Number" as appropriate. 
-    
  }
  
  function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
+    // takes in these parameters and updates shuttle requirements
+    let pilotValidation = validateInput(pilot);
+    let copilotValidation = validateInput(copilot);
+    let fuelLevelValidation = validateInput(fuelLevel);
+    let cargoLevelValidation = validateInput(cargoLevel); 
+
+    if(pilotValidation === "Empty" || copilotValidation === "Empty" ||
+        fuelLevelValidation === "Empty" ||cargoLevelValidation === "Empty"){
+            alert("All fields are required!");
+            return false;
+        }
     
+    if(fuelLevelValidation === "Not a number" ||cargoLevelValidation === "Not a number"){
+        alert("Fuel level and cargo mass must be numbers!");
+            return false;
+    }
+
+
+
  }
  
  async function myFetch() {
